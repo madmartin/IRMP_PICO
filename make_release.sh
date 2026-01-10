902f496ae1bc6d389a9a2985d1910108b9642418
Martin Dummer <martin.dummer@gmx.net>
2026-01-10 15:58:16 +0100

    make_release.sh: create archives for linux+windows binaries
    change firmware archive name from
    release.tar.xz  -> firmware_for_RP2xxx.tar.xz
    
    Signed-off-by: Martin Dummer <martin.dummer@gmx.net>

--- make_release.sh
+++ make_release.sh
@@ -1,3 +1,9 @@
-#! /bin/bash
-tar -cJf release.tar.xz  RP2xxx/build/*
+#! /bin/bash -e
+tar -cJf firmware_for_RP2xxx.tar.xz  RP2xxx/build/*
 
+# create linux binaries
+make release_linux
+
+# create windows binaries
+mkdir binaries_for_Windows || /bin/true
+zip -9 -r binaries_for_Windows.zip binaries_for_Windows/
