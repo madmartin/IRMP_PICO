521576b2667cef8f685cacf807710ec7cf8ab5e8
j1rie <j1rie@users.noreply.github.com>
2026-01-15 03:48:27 +0100

    improve make_release with inspiration from @madmartin, improve directory structure

--- make_release.sh
+++ make_release.sh
@@ -1,3 +1,11 @@
-#! /bin/bash
-tar -cJf release.tar.xz  RP2xxx/build/* binaries_for_Windows/*/* irmpalarm/Linux/irmpalarm irmpconfig/Linux/irmpconfig irmpconfig_gui/Linux/irmpconfig_gui irmpstatusled/irmpstatusled
+#! /bin/bash -e
+tar -C RP2xxx/build -cJvf firmware_for_RP2xxx.tar.xz .
 
+cd binaries_for_Windows
+zip -9 -r ../binaries_for_Windows.zip *
+cd ..
+
+mkdir tmp
+cp -p irmpalarm/Linux/irmpalarm irmpconfig/Linux/irmpconfig irmpconfig_gui/Linux/irmpconfig_gui irmpstatusled/irmpstatusled tmp/
+tar -C tmp -cJf binaries_for_Linux.tar.xz .
+rm -r tmp
